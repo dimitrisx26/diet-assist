00d1de5afd7bc25c61a9153210ca60f0070a0763
dimitrisx26 <dxynos@outlook.com>
2025-06-14 23:05:52 +0300

    Add Panos logo SVG asset to the project

--- src/app/layout/component/app.footer.ts
+++ src/app/layout/component/app.footer.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
     standalone: true,
     selector: 'app-footer',
     template: `<div class="layout-footer">
-        SAKAI by
-        <a href="https://primeng.org" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">PrimeNG</a>
+        Made by 
+        <a href="https://dxynos.com" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">Dimitrios Xynos</a>
     </div>`
 })
 export class AppFooter {}
