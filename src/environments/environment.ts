c502f3590aa53416789e9ba73614b15f72e705fc
dimitrisx26 <dxynos@outlook.com>
2025-06-15 01:46:21 +0300

    Add Supabase dependencies and service implementation

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://ecvuwhirjicjusiiptgk.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjdnV3aGlyamljanVzaWlwdGdrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk5Mzk3NDgsImV4cCI6MjA2NTUxNTc0OH0.VRded9qheZKl8bMhbmEVBMm701ehY6X1UvtFPr7CIe8'
+};
